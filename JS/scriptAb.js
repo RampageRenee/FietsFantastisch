3e825a0728d799184305f9587d859b627a5a4ac9
Abboukari <71227631+Abboukari@users.noreply.github.com>
2020-10-26 17:24:27 +0100

    changes

---
+++ JS/scriptAb.js
@@ -0,0 +1,13 @@
+var klant;
+klant = [Klaas, Jan, Peter, Karel, Maaike];
+
+var medewerker;
+medewerker = [Ab, Tarik, Dia, Fili, Renee];
+
+var fiets;
+fiets = [herenfiets, damesfiets, elektrischefiets, elektrischexfiets, elektrischesuperfiets]; 
+
+var accessoire;
+accessoire = [zijwieltjes, helm, kniebeschermers,  ]
+
+
