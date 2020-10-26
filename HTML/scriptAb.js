23db31d8f48f14a5351400ef81a38f65b910f8a8
Abboukari <71227631+Abboukari@users.noreply.github.com>
2020-10-26 18:12:02 +0100

    js changes

---
+++ HTML/scriptAb.js
@@ -0,0 +1,22 @@
+
+var klant;
+klant = ['Klaas', 'Jan', 'Peter', 'Lisa', 'Maaike'];
+
+console.log(klant);
+
+
+var medewerker;
+medewerker = ['Ab', 'Tarik', 'Pieter', 'Fili', 'Renee'];
+
+console.log(medewerker);
+
+var fiets;
+fiets = ['herenfiets', 'damesfiets', 'elektrischefiets', 'elektrischexfiets', 'elektrischesuperfiets']; 
+
+console.log(fiets);
+
+var accessoire;
+accessoire = ['zijwieltjes', 'helm', 'kniebeschermers',  'kinderzitje', 'elleboogbeschermers'];
+
+console.log(accessoire);
+
