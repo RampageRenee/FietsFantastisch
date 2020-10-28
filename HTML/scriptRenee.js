d6adbeb0c047b484379ea2288c6f846318709585
SupeRenee <reneeclerx@gmail.com>
2020-10-28 20:08:40 +0100

    beeetje kloootten

--- HTML/scriptRenee.js
+++ HTML/scriptRenee.js
@@ -181,3 +181,27 @@ function GetMap() {
 
 
 /*--------------------------------------------------LOG IN -----------------------------------------------------------*/
+
+const box = document.getElementById("passzichtbaar");
+
+box = console.log("nu dan??");
+
+//box.addEventListener("change" ,function(){
+  //  let veld = document.getElementById("veld2");
+
+    //if(true){
+      //  veld.type = "text"
+        //console.log('ik ben binnen');
+    //}
+//})
+
+box.addEventListener("change", e=> {
+    let veld = document.getElementById("veld2");
+    
+    veld.type = e.target.checked ? "text": "password"
+    
+    //if (e.target.checked){
+      //  veld.type = "text"
+
+    //}
+})
