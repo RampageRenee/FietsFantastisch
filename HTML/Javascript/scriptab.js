20a82ca86c1cb47243df952a61aa6a18505c2d2b
Abboukari <71227631+Abboukari@users.noreply.github.com>
2020-11-06 21:40:36 +0100

    dropdown menu

--- HTML/Javascript/scriptab.js
+++ HTML/Javascript/scriptab.js
@@ -1,6 +1,4 @@
 
-
-
 function Fietsen(merk,type,soort,hoogte,aantal){
     this.merk = merk;
     this.type = type;
@@ -11,21 +9,25 @@ function Fietsen(merk,type,soort,hoogte,aantal){
 
 // this.foto  
 
-
-var soortenFietsen = [
+let soortenFietsen = [
     new Fietsen("Van Moof", "X01", "Heren fiets",  62, 12),
     new Fietsen("Koga", "HJC3", "Dames fiets", 51, 6),
     new Fietsen("Speedstep", "Model X", "Elektrische Fiets", 62, 8),
-    new Fietsen ("Speedstep", "Model Y","Elektrische Super Fiets", 62, 4),
-    new Fietsen ("Speedstep", "Model Z", "Elektrische Super X Fiets",62, 1)
+    new Fietsen ("SpeedstepS", "Model Y","Elektrische Super Fiets", 62, 4),
+    new Fietsen ("SpeedstepX", "Model Z", "Elektrische Super X Fiets",62, 1)
 ]; 
 
 
-console.log(soortenFietsen[2].merk);
+// for loop gebruiken om in de dropdown menu het merk van de objecten uit de array
+// weer te geven van het soort fiets
+
+var select = document.getElementById("select");
+
+for (var i = 0; i < soortenFietsen.length; i++){
+    select[select.length]= new Option(soortenFietsen[i].merk);
+};
+
+console.log(soortenFietsen[1]);
+
 
 // event select waarvoor te gebruiken 
-// 
-
-
-// Geen idee hoe de dropdown menu te creeren in javascript, staat hier ergens meer uitleg over?
-// Kan het niet terug vinden in de linked in course of de aangereikte matriaalen
