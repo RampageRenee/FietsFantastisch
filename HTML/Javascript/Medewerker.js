334e4928296b9be96462ef23b1db8a32d636691e
SupeRenee <reneeclerx@gmail.com>
2020-11-15 12:10:01 +0100

    plaatje js en css

--- HTML/Javascript/Medewerker.js
+++ HTML/Javascript/Medewerker.js
@@ -2,13 +2,6 @@
 /*--------------------------------------------------MEDEWERKERS----------MADE BY RENEE-------------------------------------------------*/
 
 
-function Klant(naam, voornaam, klantnummer, adres,) {
-    this.naam = naam;
-    this.voornaam = voornaam;
-    this.klantnummer = klantnummer;
-    this.adres = adres;
-
-}
 
 function Medewerker(naam, nummer, functie, afbeelding) {
     this.naam = naam;
@@ -18,20 +11,12 @@ function Medewerker(naam, nummer, functie, afbeelding) {
 }
 
 
-var klanten = [
-    new Klant("Weasley", "Ron", 1, "Den Haag"),
-    new Klant("Skywalker", "Pad-Me", 2, "Rijswijk"),
-    new Klant("Kent", "Clark", 3, "Zoetermeer"),
-    new Klant("Prince", "Diana", 4, "Wassenaar"),
-    new Klant("Sanchez", "Rick", 5, "Scheveningen"),
-]
-
 var medewerkers = [
-    new Medewerker("Ikora", 236, "Directeur","HTML\images\MED Ikora Rey.jpg"),
-    new Medewerker("Cayde-6", 006, "Monteur", "HTML\images\MED Cayde-6, _.jpg"),
-    new Medewerker("Zavala", 237, "Balie", "HTML\images\MED Zavala.jpg"),
-    new Medewerker("Lord Shaxx", 332, "Monteur", "HTML\images\MED Lord Shaxx.jpg"),
-    new Medewerker("Xur", 159, "Balie", "HTML\images\MED Where Is Xur.jpg")
+    new Medewerker("Ikora", 236, "Directeur","./images/MED Ikora Rey.jpg"),
+    new Medewerker("Cayde-6", 006, "Monteur", "./images/MEDCayde-6.jpg"),
+    new Medewerker("Zavala", 237, "Balie", "./images/MED Zavala.jpg"),
+    new Medewerker("Lord Shaxx", 332, "Monteur", "./images/MED Lord Shaxx.jpg"),
+    new Medewerker("Xur", 159, "Balie", "./images/MED Where Is Xur.jpg")
 ]
 
 
@@ -60,23 +45,23 @@ function lijstSelectie(z){
     var pony = medewerkers[z].naam;
     var paard = medewerkers[z].nummer;
     var eekhoorn = medewerkers[z].functie;
+    
     document.getElementById('medewerker-naam').innerHTML = pony;
     document.getElementById('medewerker-nummer').innerHTML = paard;
     document.getElementById('medewerker-functie').innerHTML = eekhoorn;
     
 }
 
+function fotoSelectie(z){
+    var plaatje = medewerkers[z].afbeelding;
+    document.getElementById('afbeeldingMedewerker').src =plaatje;
+}
 
 
 
 //-----------------dropdown loop met toekomst op dynamiek
 for (var i = 0; i < medewerkers.length; ++i) {
-    /*let a = medewerkers[i].naam;
-    let b = medewerkers[i];
-    keuzeBalk[keuzeBalk.length] = new Option(a, b);
-    console.log(a);
-    console.log(b);
-    var z= a;*/
+   
 
     var newpickelement = document.createElement("option");
     var newpicktext = document.createTextNode(medewerkers[i].naam);
@@ -87,20 +72,11 @@ for (var i = 0; i < medewerkers.length; ++i) {
 
     console.log(newpickelement);
 
-    /*function Jeezus (){
-    addEventListener('select' ,function(){
-        var pakLijstNummer = z;
-        return pakLijstNummer;
-    })
-    }*/
-
 }
 
 
 
 keuzeBalk.addEventListener('change', function () {
-    
-
 
     var e = document.getElementById("medewerkerDropDown");
     var result = e.options[e.selectedIndex].value;
@@ -108,9 +84,8 @@ keuzeBalk.addEventListener('change', function () {
     console.log(result);
 
     lijstSelectie(result);
+    fotoSelectie(result);
     
-    
-
 })
 
 
@@ -119,9 +94,6 @@ keuzeBalk.addEventListener('change', function () {
 
 //------------------------Maak een nieuwe medewerker aan--------------------------
 
-var deNaam = document.getElementById('med-naam-input').value;
-var hetNummer = document.getElementById('med-nummer-input').value;
-var deFunctie = document.getElementById('med-functie-input').value;
 
 
 
@@ -153,10 +125,15 @@ function voegtoe(e) {
     }
     else {
         medewerkers.push(new Medewerker(deNaam, hetNummer, deFunctie));
-        window.alert("Medewerker:" + deNaam + " nummer:" + hetNummer + "functie:" + deFunctie + " is toegevoegd.");
+        window.alert("Medewerker: " + deNaam + ", nummer: " + hetNummer + ", functie: " + deFunctie + " is toegevoegd.");
     }
 }
 
+
+
+var deNaam = document.getElementById('med-naam-input').value;
+var hetNummer = document.getElementById('med-nummer-input').value;
+var deFunctie = document.getElementById('med-functie-input').value;
 
 
 document.getElementById("med-verzendknop").onclick = voegtoe;
