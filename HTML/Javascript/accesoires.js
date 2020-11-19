3bb6d907d0ea9d20b6731ba74c8e1eeea2c389c3
tarik <Tarikdriss@outlook.com>
2020-11-19 18:59:36 +0100

    ti

---
+++ HTML/Javascript/accesoires.js
@@ -0,0 +1,9 @@
+// script door tarik
+// constructor voor accesoires
+
+function accesoires(merk,soort,type,image){
+    this.merk = merk;
+    this.soort = soort;
+    this.type = type;
+    this.image = image;
+}
