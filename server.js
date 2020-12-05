1cc80c99e50835c6edcc24fc385acde784675b92
SupeRenee <reneeclerx@gmail.com>
2020-12-05 21:09:07 +0100

    medewerkers

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 8000;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "./vieuws"));
+app.set("views", path.join(__dirname, "./views"));
 
 app.use(express.static(path.join(__dirname, "./HTMLNode")));
 
