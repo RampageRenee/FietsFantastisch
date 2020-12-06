b35583984bae5dc095b6ec81abad552fcadd1309
SupeRenee <reneeclerx@gmail.com>
2020-12-06 13:48:18 +0100

    verwijzingen naar lijsten voor week 2 en 3

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 
+const medewerkersRoute = require("./medewerkers");
+/* const fietsenRoute
+const klantenRoute
+const accsoiresRoute */
+
 const router = express.Router();
 
-module.exports = () => {
+module.exports = (params) => {
   router.get("/", (request, response) => {
     response.render("paginas/index", { paginaTitel: "Welkom" });
   });
+
+  router.use("/medewerkers", medewerkersRoute(params));
+  /* fietsenRoute()
+  klantenRoute()
+  accesoiresRoute() */
 
   return router;
 };
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const path = require("path");
 
+const MedewerkerService = require("./services-control/MedewerkerService");
+/* const KlantenService
+const FietsenService
+const AccesoiresService */
+
+const medewerkerService = new MedewerkerService("./data/medewerkers.json");
+/* const klantenService = new klantenService
+const fietsenService
+const accsoiresService  */
+
+const routes = require("./routes");
+
 const app = express();
 
 const port = 8000;
@@ -9,6 +21,16 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 
 app.use(express.static(path.join(__dirname, "./HTMLNode")));
+
+app.use(
+  "/",
+  routes({
+    medewerkerService,
+    /* klantenService,
+  fietsenService,
+  accecoiresService, */
+  })
+);
 
 app.listen(port, () => {
   console.log(`Express server listning on port ${port}!`);
